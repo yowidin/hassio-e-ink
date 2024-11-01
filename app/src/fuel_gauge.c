5e90638c4cb1b33326d0efff9551f3e0c8a06789
YoWiDiN <yowidin@gmail.com>
2024-11-01 09:08:43 +0100

    Clean up the fuel gauge API

--- app/src/fuel_gauge.c
+++ app/src/fuel_gauge.c
@@ -24,13 +24,27 @@ bool hei_fuel_gauge_init(void) {
 }
 
 void hei_fuel_gauge_print(void) {
-   if (!device_is_ready(fuel_gauge)) {
-      LOG_WRN("Fuel gauge not ready: %s", fuel_gauge->name);
+   const hei_fuel_gauge_measurement_t mes = hei_fuel_gauge_get();
+   if (mes.valid) {
       return;
    }
 
+   LOG_INF("Time to empty %" PRIu32, mes.runtime_to_empty_minutes);
+   LOG_INF("Time to full %" PRIu32, mes.runtime_to_full_minutes);
+   LOG_INF("Charge  %" PRIu8 "%%", mes.relative_state_of_charge_percentage);
+   LOG_INF("Voltage %d", mes.voltage_uv);
+}
+
+hei_fuel_gauge_measurement_t hei_fuel_gauge_get(void) {
+   if (!device_is_ready(fuel_gauge)) {
+      LOG_WRN("Fuel gauge not ready: %s", fuel_gauge->name);
+      return (hei_fuel_gauge_measurement_t){.valid = false};
+   }
+
+   // This is everything, MAX17048 supports (at least it's driver doesn't support anything else at the moment).
    fuel_gauge_prop_t props[] = {
       FUEL_GAUGE_RUNTIME_TO_EMPTY,
+      FUEL_GAUGE_RUNTIME_TO_FULL,
       FUEL_GAUGE_RELATIVE_STATE_OF_CHARGE,
       FUEL_GAUGE_VOLTAGE,
    };
@@ -40,9 +54,20 @@ void hei_fuel_gauge_print(void) {
    int err = fuel_gauge_get_props(fuel_gauge, props, vals, ARRAY_SIZE(props));
    if (err < 0) {
       LOG_ERR("Fuel gauge read failed");
-   } else {
-      LOG_INF("Time to empty %" PRIu32, vals[0].runtime_to_empty);
-      LOG_INF("Charge  %" PRIu8 "%%", vals[1].relative_state_of_charge);
-      LOG_INF("Voltage %d", vals[2].voltage);
+      return (hei_fuel_gauge_measurement_t){.valid = false};
    }
+
+   const int voltage = vals[3].voltage;
+   if (voltage < 0) {
+      LOG_ERR("Unexpected negative voltage: %d", voltage);
+      return (hei_fuel_gauge_measurement_t){.valid = false};
+   }
+
+   return (hei_fuel_gauge_measurement_t){
+      .valid = true,
+      .runtime_to_empty_minutes = vals[0].runtime_to_empty,
+      .runtime_to_full_minutes = vals[1].runtime_to_full,
+      .relative_state_of_charge_percentage = vals[2].relative_state_of_charge,
+      .voltage_uv = (uint32_t)voltage,
+   };
 }
